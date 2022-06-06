afad2c302620dbf3ca7d1985701b6d098b18c27c
Andrey Sibrinin <sibrinin.andrei@gmail.com>
2022-06-06 17:37:20 +0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import {Route, Routes} from "react-router-dom";
+import Layout from "./components/Layout";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      test
-    </div>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+              <Route index element={<Main/>} />
+              <Route path="/login" element={<Login/>} />
+          </Route>
+      </Routes>
   );
 }
 
---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import {Outlet} from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <>
+            <header>
+                header
+            </header>
+            <main>
+                <Outlet/>
+            </main>
+        </>
+    );
+};
+
+export default Layout;
